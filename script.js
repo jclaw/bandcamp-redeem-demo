85b60e404b5e0912b16482bd59033921d2bf2257
jclaw <jclaw3456@gmail.com>
2016-06-23 12:00:57 -0500

    desktop done

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-	
+	$('#log-in').click(function() {
+		login();
+	})
+
+	$('.bc-ui.gear').click(function() {
+		logout();
+	})
+
+	function login() {
+		$('body').addClass('loggedin');
+		console.log('logged in');
+	}
+
+	function logout() {
+		$('body').removeClass('loggedin');
+		console.log('logged out');
+	}
 })
