70add7ee968662f7801f1c8ec3ecfa27f7196dd8
jclaw <jclaw3456@gmail.com>
2016-06-27 14:30:01 -0500

    implemented responsive content changes

--- script.js
+++ script.js
@@ -1,56 +1,8 @@
-var state;
-
-// $('head').ready(function() {
-// 	var width = $(window).width();
-// 	if (width <= 740) {
-// 		state = 'mobile';
-// 		console.log('mobile');
-// 		loadCSS('tralbum_mobile_bundle.css');
-// 		loadCSS('global_phone_bundle.css');
-//
-// 	} else {
-// 		state = 'desktop';
-// 		console.log('desktop');
-// 		loadCSS('tralbum_bundle.css');
-// 		loadCSS('global_bundle.css');
-// 	}
-//
-// 	function loadCSS(href) {
-// 		var cssLink = $("<link rel='stylesheet' type='text/css' href='"+href+"'>");
-// 		$("head").prepend(cssLink);
-// 	};
-// })
-
 $(document).ready(function() {
-	var responsiveData = [
-		{
-			selector: '#code label',
-			desktop: 'Code',
-			mobile: 'code:'
-		}, {
-			selector: '#email label',
-			desktop: 'Email <span class="de-emphasized">(optional)</span>',
-			mobile: 'email:'
-		}, {
-			selector: '#email .form-field .textinput',
-			desktop: "join the artist's mailing list",
-			mobile: 'download link will be sent here'
-		}
-	];
+
 	var iconTimer;
 
-	// populate html with correct content
-	$.each(responsiveData, function(index, obj) {
-		var element = $(obj.selector);
-		if (element.is('input')) {
-			element.attr('placeholder',obj[state]);
-		} else {
-			element.html(obj[state]);
-		}
-	})
-
-
-	$('.small-message a').click(function() {
+	$('.small-message a, #login-main a').click(function() {
 		login();
 		return false;
 	})
@@ -82,13 +34,27 @@ $(document).ready(function() {
 			clearTimeout(iconTimer);
 			icon.removeClass('loading times check');
 		}
-
 	})
 
-	$('#redeem-form').submit(function() {
+	// maintain same value in both email input fields
+	// there are two email input fields so that each can have a
+	// different placeholder
+	$('.email-input').on('input', function() {
+		var el = $(this);
+		$('.email-input').not(el).val(el.val());
+	})
+
+	$('#redeem-form').submit(function(e) {
+		e.preventDefault();
 		var form = $(this);
 		var code = form.find('#code-input').val();
-		var email = form.find('#email-input').val();
+		var emailfields = form.find('.email-input');
+		var email;
+		if ($(emailfields[0]).val() == '') {
+			email = $(emailfields[1]).val();
+		} else {
+			email = $(emailfields[0]).val();
+		}
 		console.log(code, email);
 	})
 
